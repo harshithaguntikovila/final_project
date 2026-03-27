61aa3c3b84802df517033c240bc81d3f9319f080
harshithaguntikovila <guntikovilaharshitha@gmail.com>
2026-03-27 22:25:39 +0530

    Fixed base for both dev and production

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,20 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/final_project/',   
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/final_project/',   
+  base: process.env.NODE_ENV === 'production'
+    ? '/final_project/'
+    : '/',
   plugins: [react()],
 })
